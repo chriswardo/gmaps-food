d44d4fd8c11471046e8aa00b1b56b49c41e63c17
chriswardo <chris@chrisward.co.uk>
2016-11-16 09:48:35 +0700

    v1

--- src/gmaps2json.sh
+++ src/gmaps2json.sh
@@ -7,6 +7,7 @@ if [ "$2" != "" ]; then
   html-minifier --collapse-whitespace out/$1.max.html -o out/$1.html
   gzip -9 -f --keep out/$1.html
   
+  cp templates/styles/style.less out/styles/style.less
   lessc out/styles/style.less out/styles/style.min.css --clean-css="--s1 --advanced --compatibility=ie8"
   #curl -s https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css >> out/styles/style.min.css
   #curl -s http://fonts.googleapis.com/css?family=Roboto:400,500,300 >> out/styles/style.min.css
@@ -14,6 +15,7 @@ if [ "$2" != "" ]; then
   #cat out/styles/swipebox.min.css >> out/styles/style.min.css
   gzip -9 -f --keep out/styles/style.min.css
   
+  cp templates/scripts/script.js out/scripts/script.js
   minify out/scripts/script.js
   #curl -s https://code.jquery.com/jquery-2.1.3.min.js > out/scripts/combined.min.js
   #cat out/scripts/jquery.swipebox.min.js >> out/scripts/combined.min.js
